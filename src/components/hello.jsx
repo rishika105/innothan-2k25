da1df9422495f3cc201a08c80d669d6963ed8c5e
Rishika Agarwal <rishikaagarwal2316@gmail.com>
2025-01-30 20:39:54 +0530

    branch rishika

---
+++ src/components/hello.jsx
@@ -0,0 +1,10 @@
+const Hello = () => {
+  return (
+    <>
+      <div className="text-white text-5xl">This is hackathon practice 2025</div>
+      <div>From my branch</div>
+    </>
+  );
+};
+
+export default Hello;
