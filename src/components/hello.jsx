53e183effd5bc97d20b06c5a4b3d95514c28ef74
arpita-32 <arpitasundaray9@gmail.com>
2025-01-30 21:09:17 +0530

    Updated hello.jsx

--- src/components/hello.jsx
+++ src/components/hello.jsx
@@ -1,9 +1,7 @@
 const Hello = () => {
   return (
-    <>
-      <div className="text-white text-5xl">This is hackathon practice 2025</div>
-      <div>From my branch</div>
-    </>
+    <div className="text-white text-5xl">This is hackathon practice 2025</div>
+    
   );
 };
 
